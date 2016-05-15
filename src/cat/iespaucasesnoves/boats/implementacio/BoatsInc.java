935f310b1dd9aa9bb312abea01ea24a0e0e63ead
DRHAKUL <drakul_x@yahoo.com>
2016-05-15 09:37:15 +0200

    Merge origin/master
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/cat/iespaucasesnoves/boats/implementacio/BoatsInc.java
@@ -0,0 +1,13 @@
+package cat.iespaucasesnoves.boats.implementacio;
+
+/**
+ *
+ * @author DRAKUL
+ */
+public class BoatsInc {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
