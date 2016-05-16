365a1817bb1e73a7f98e2efa5175222808825423
DRHAKUL <drakul_x@yahoo.com>
2016-05-16 20:22:57 +0200

    Merge origin/master
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/cat/iespaucasesnoves/boats/api/Persona.java
+++ src/cat/iespaucasesnoves/boats/api/Persona.java
@@ -1,17 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cat.iespaucasesnoves.boats.api;
 
-/**
- *
- * @author jorge
- */
 public class Persona {
+
     protected String nom;
     protected String cognom;
-    protected 
-    
+    protected Document tipusDocument;
+    protected String adreca;
+    protected String telefon;
+    protected String correu;
+
+    public Persona(String nom, String cognom, Document tipusDocument, String adreca, String telefon, String correu) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.tipusDocument = tipusDocument;
+        this.adreca = adreca;
+        this.telefon = telefon;
+        this.correu = correu;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public Document getTipusDocument() {
+        return tipusDocument;
+    }
+
+    public void setTipusDocument(Document tipusDocument) {
+        this.tipusDocument = tipusDocument;
+    }
+
+    public String getAdreca() {
+        return adreca;
+    }
+
+    public void setAdreca(String adreca) {
+        this.adreca = adreca;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getCorreu() {
+        return correu;
+    }
+
+    public void setCorreu(String correu) {
+        this.correu = correu;
+    }
+
 }
