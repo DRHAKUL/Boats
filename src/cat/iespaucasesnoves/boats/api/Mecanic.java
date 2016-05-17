db46a257a08cc54716b35df2eacb2d97f9da04dc
DRHAKUL <drakul_x@yahoo.com>
2016-05-17 15:35:30 +0200

    Merge origin/master
    Conflicts:
    	src/cat/iespaucasesnoves/boats/api/Client.java

---
+++ src/cat/iespaucasesnoves/boats/api/Mecanic.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.iespaucasesnoves.boats.api;
+
+/**
+ *
+ * @author DRAKUL
+ */
+public class Mecanic extends Empleat {
+
+    public Mecanic(String nom, String cognom, Document tipusDocument, String adreca, String telefon, String correu) {
+        super(nom, cognom, tipusDocument, adreca, telefon, correu);
+    }
+
+}
