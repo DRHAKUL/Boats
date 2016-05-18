8ecaa5a9336273bef16ba5627f5b9c962de9270b
DRHAKUL <drakul_x@yahoo.com>
2016-05-18 16:54:26 +0200

    Merge origin/master
    Conflicts:
    	src/cat/iespaucasesnoves/boats/api/Client.java

--- src/cat/iespaucasesnoves/boats/api/Venedor.java
+++ src/cat/iespaucasesnoves/boats/api/Venedor.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cat.iespaucasesnoves.boats.api;
 
 import java.util.Date;
 
-/**
- *
- * @author DRAKUL
- */
 public class Venedor extends Empleat {
 
     private double comissio;
