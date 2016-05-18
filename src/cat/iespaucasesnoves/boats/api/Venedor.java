57a9e6f0a354198f911f56976ffac75bd96ca2d6
DRAKUL <DRAKUL@DRAKUL-PC>
2016-05-18 23:02:40 +0200

    Estructura principal

--- src/cat/iespaucasesnoves/boats/api/Venedor.java
+++ src/cat/iespaucasesnoves/boats/api/Venedor.java
@@ -6,10 +6,12 @@ import cat.iespaucasesnoves.boats.exepcions.PersonaException;
 import cat.iespaucasesnoves.boats.exepcions.VenedorException;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Venedor extends Empleat {
 
     private double comissio;
+    private double souTotal;
     private HashMap<Integer, Venda> llistaVendes;
 
     public Venedor(double comissio, String nom, String cognom, Document tipusDocument, String numeroDocument, String adreca, String telefon, String correu, Date dataAlta, Double sou) throws VenedorException, DadesIncorrectesException, PersonaException, EmpleatException {
@@ -20,6 +22,7 @@ public class Venedor extends Empleat {
         } else {
             this.comissio = comissio;
         }
+        souTotal = 0;
     }
 
     public double getComissio() {
@@ -34,8 +37,23 @@ public class Venedor extends Empleat {
         }
     }
 
+    public void setSouTotal() {
+        Iterator it = llistaVendes.entrySet().iterator();
+        double totalVendes = 0;
+        while (it.hasNext()) {
+            Venda nova = (Venda) it.next();
+            Venedor mostra = nova.getVenedor();
+            if (mostra.getNumeroDocument().equals(this.getNumeroDocument())) {
+                totalVendes = totalVendes + nova.getPreu();
+
+            }
+        }
+
+        souTotal = sou + (totalVendes * 100 / comissio);
+    }
+
     public String getNomina() {
-        return "{nom:" + nom + ",cognom:" + cognom + ",tipusDocument:" + tipusDocument + ",numeroDocument:" + numeroDocument + ",adreca:" + adreca + ",telefon:" + telefon + ",correu:" + correu + ",dataAlta:" + dataAlta + ",sou:" + sou + ",comissio" + comissio + "}";
+        return "{nom:" + nom + ",cognom:" + cognom + ",tipusDocument:" + tipusDocument + ",numeroDocument:" + numeroDocument + ",adreca:" + adreca + ",telefon:" + telefon + ",correu:" + correu + ",dataAlta:" + dataAlta + ",sou:" + sou + ",comissio:" + comissio + ",souTotal:" + souTotal + "}";
     }
 
 }
