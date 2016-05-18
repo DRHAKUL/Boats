f9ac1dba7256ba6ef81e102ca1a83fcbe27f5940
DRHAKUL <drakul_x@yahoo.com>
2016-05-18 18:47:54 +0200

    Merge origin/master
    Conflicts:
    	src/cat/iespaucasesnoves/boats/api/Client.java

--- src/cat/iespaucasesnoves/boats/api/Lloguer.java
+++ src/cat/iespaucasesnoves/boats/api/Lloguer.java
@@ -15,23 +15,65 @@ public class Lloguer extends Operacio {
 
     private boolean ambPatro;
     private Date iniciLloguer;
+    private Date fiLloguer;
     private Client client;
     private Vaixell vaixell;
     private double preuDia;
     private Patro patro;
 
-    public Lloguer(boolean ambPatro, Date iniciLloguer, Client client, Vaixell vaixell, double preuDia, Patro patro, Estat estat) {
+    public Lloguer(boolean ambPatro, Date iniciLloguer, Date fiLloguer, Client client, Vaixell vaixell, double preuDia, Patro patro, Estat estat) {
         super(estat);
         this.ambPatro = ambPatro;
         this.iniciLloguer = iniciLloguer;
+        this.fiLloguer = fiLloguer;
         this.client = client;
         this.vaixell = vaixell;
         this.preuDia = preuDia;
         this.patro = patro;
     }
 
-    public Lloguer(Date iniciLloguer, Client client, Vaixell vaixell, double preuDia, Estat estat) {
-        this(false, iniciLloguer, client, vaixell, preuDia, null, estat);
+    public Lloguer(Date iniciLloguer, Date fiLloguer, Client client, Vaixell vaixell, double preuDia, Estat estat) {
+        this(false, iniciLloguer, fiLloguer, client, vaixell, preuDia, null, estat);
+    }
+
+    public boolean isAmbPatro() {
+        return ambPatro;
+    }
+
+    public Date getIniciLloguer() {
+        return iniciLloguer;
+    }
+
+    public Date getFiLloguer() {
+        return fiLloguer;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public Vaixell getVaixell() {
+        return vaixell;
+    }
+
+    public double getPreuDia() {
+        return preuDia;
+    }
+
+    public Patro getPatro() {
+        return patro;
+    }
+
+    public void setIniciLloguer(Date iniciLloguer) {
+        this.iniciLloguer = iniciLloguer;
+    }
+
+    public void setFiLloguer(Date fiLloguer) {
+        this.fiLloguer = fiLloguer;
+    }
+
+    public void setPreuDia(double preuDia) {
+        this.preuDia = preuDia;
     }
 
 }
