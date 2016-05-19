261320ca52f65407425819c4648a08b3496f9bc0
Carlos <Carlos@Portatil>
2016-05-19 17:48:28 +0200

    Merge origin/master
    Conflicts:
    	src/cat/iespaucasesnoves/boats/api/Model.java

---
+++ src/cat/iespaucasesnoves/boats/proves/Proves.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.iespaucasesnoves.boats.proves;
+import cat.iespaucasesnoves.boats.api.Boats;
+/**
+ *
+ * @author Carlos
+ */
+public class Proves {
+
+    public void inicialitzarProves(){
+        Boats principal = new Boats();
+        
+        
+        
+    }
+    
+    public void crearFitxer(){
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
