4bcb196003fb95deaf710154d15bdcfbe9b45e49
DRHAKUL <drakul_x@yahoo.com>
2016-05-25 18:39:49 +0200

    Merge origin/master
    Conflicts:
    	Boats.boa
    	src/cat/iespaucasesnoves/boats/api/Boats.java

--- src/cat/iespaucasesnoves/boats/api/Compte.java
+++ src/cat/iespaucasesnoves/boats/api/Compte.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cat.iespaucasesnoves.boats.api;
 
 import cat.iespaucasesnoves.boats.exepcions.CompteException;
@@ -13,7 +8,7 @@ import java.io.Serializable;
  *
  * @author jorge
  */
-public class Compte implements Pagable,Serializable {
+public class Compte implements Pagable, Serializable {
 
     private String iban;
 
